f970e8376fb6e7acc9d0256058a7531565ca0957
Michael Seaward <mamoplays@gmail.com>
2019-05-03 17:00:06 -0300

    build: boilerplate, needs test-cases
    react, sass, webpack, babel, eslint, stylelint boilerplate project. Still needs test cases
    configured. USE NODE 10.15.x

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <p className='code-me'>
+        CODE ME!
+      </p>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/App/index.js
@@ -0,0 +1,4 @@
+import App from './App';
+import './App.scss';
+
+export default App;
