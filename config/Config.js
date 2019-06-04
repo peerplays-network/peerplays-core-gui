61cc964cbab3d6c7a24df48730a52144e8abda67
mseaward <36667230+mseaward@users.noreply.github.com>
2019-06-04 16:29:52 -0300

    chore: enable differing uia for different chains (#43)
    * chore: enable differing uia for different chains
    
    WAL-236

--- config/Config.js
+++ config/Config.js
@@ -6,7 +6,6 @@ const env = getClientEnvironment();
 const blockchainUrls = env.raw.apiEndpoints;
 // Get the faucet endpoints available.
 const faucetUrls = env.raw.faucetUrls;
-
 // Shuffle the available enpoints.
 const shuffledFaucetUrls = _.shuffle(faucetUrls);
 
@@ -17,7 +16,7 @@ const Config = {
   FAUCET_URLS: shuffledFaucetUrls,
 
   IDLE_TIMEOUT: 600000, // Time is in milliseconds.
-  CORE_ASSET: 'PPY',
+  CORE_ASSET: env.raw.prefix,
   FAUCET_URL: 'faucetUrls',
   BITSHARES_WS: 'wss://bitshares.openledger.info/ws',
   SOFTWARE_UPDATE_REFERENCE_ACCOUNT_NAME: 'ppcoreupdates',
@@ -28,7 +27,7 @@ const Config = {
     sortingMethod: 'recent', // recent OR oldest
     timeout: 7500, // 7.5 seconds for auto-dismissal messages
     disableActionsInRedux: true
-  },
+  }
 };
 
 module.exports = Config;
