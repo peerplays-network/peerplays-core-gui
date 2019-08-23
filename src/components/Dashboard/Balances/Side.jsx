4fc545af257cbdef1a01162b70ee0b456dcaa603
mamokin <mamokinmedia@gmail.com>
2019-08-23 17:24:49 -0300

    feat(gposPanel): gpos balance less than zero view
    WAL-247

--- src/components/Dashboard/Balances/Side.jsx
+++ src/components/Dashboard/Balances/Side.jsx
@@ -8,6 +8,7 @@ import asset_utils from '../../../common/asset_utils';
 import AppActions from '../../../actions/AppActions';
 import SideVesting from './SideVesting';
 import {bindActionCreators} from 'redux';
+import GPOSPanel from '../../GPOSPanel';
 class Side extends React.Component {
 
   componentWillMount() {
@@ -101,6 +102,9 @@ class Side extends React.Component {
               </div>
             );
           })}
+        </div>
+        <div className='aside__item bb'>
+          <GPOSPanel/>
         </div>
         <SideVesting/>
       </aside>
---
+++ src/components/GPOSPanel/GPOSPanel.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import Translate from 'react-translate-component';
+import {HelpActions} from '../../actions';
+// import {translate} from 'counterpart';
+
+class GPOSPanel extends Component {
+  onClickHelpLearn = (e) => {
+    this.props.toggleHelpModal(true);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='gpos-panel'>
+        <Translate
+          component='div'
+          className='aside__title'
+          content='gpos.side.title'
+        />
+        <div className='gpos-panel__desc'>
+          <Translate
+            component='p'
+            className='gpos-panel__desc-txt'
+            content='gpos.side.desc'
+          />
+          <Translate
+            component='a'
+            className='gpos-panel__desc-link'
+            content='gpos.side.learn'
+            onClick={ this.onClickHelpLearn }
+          />
+        </div>
+        <button
+          type='button'
+          className='btn btn-content__head'
+          // onClick={ }
+        >
+          <Translate content='gpos.side.start'/>
+        </button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => { // eslint-disable-line
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(
+  {
+    toggleHelpModal: HelpActions.toggleHelpModal
+  },
+  dispatch
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(GPOSPanel);
