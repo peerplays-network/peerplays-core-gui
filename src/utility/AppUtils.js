360cbda009b7cf376cfb72857371aaacb617c5e5
mamokin <mamokinmedia@gmail.com>
2019-08-26 17:35:39 -0300

    refactor(helpmodal): refactor scroll refs
    more universal solution that allows text links to scroll to a ref in another component

--- src/utility/AppUtils.js
+++ src/utility/AppUtils.js
@@ -14,6 +14,15 @@ const AppUtils = {
   // Check if the app is running inside electron
   isRunningInsideElectron() {
     return window && window.process && window.process.type === 'renderer';
+  },
+  /**
+   * Scroll to a `ref` within a HTML element.
+   *
+   * @param {HTMLElement} targetRef - the ref in the parent element to scroll to.
+   * @param {HTMLElement} parentElement - provides context to targetRef
+   */
+  scrollToRef(targetRef, parentElement) {
+    parentElement.scrollTop = targetRef.offsetTop - parentElement.offsetTop;
   }
 };
 
