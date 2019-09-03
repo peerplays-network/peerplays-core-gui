7d3d0da240ab9a6fff79c1c51ce00ee244301655
mseaward <m.seaward@pbsa.info>
2019-09-03 10:18:05 -0300

    refactor: remove commented lines

--- src/assets/loader.js
+++ src/assets/loader.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-// require('file-loader?name=index.html!../../public/index.html');
-// require('file-loader?name=favicon.ico!./favicon.ico');
 require('file-loader?name=dictionary.json!../common/dictionary_en.json');
 require('babel-polyfill');
 require('whatwg-fetch');
@@ -8,13 +6,3 @@ require('indexeddbshim');
 require('./asset-symbols/symbols.js');
 require('./images/images.js');
 require('./locales/locales.js');
-
-
-// require("./stylesheets/app.scss");
-// require("file-loader?name=favicon.ico!./favicon.ico");
-// require("babel-polyfill");
-// require("whatwg-fetch");
-// require("indexeddbshim");
-// require("./asset-symbols/symbols.js");
-// require("./images-loader.js");
-// require("./language-dropdown/flags.js");
