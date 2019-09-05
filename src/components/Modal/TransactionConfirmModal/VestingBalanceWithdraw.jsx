c719092849a2d93169b865524e7b92f4bc6968f0
mseaward <m.seaward@pbsa.info>
2019-09-05 15:59:32 -0300

    chore: remove console log

--- src/components/Modal/TransactionConfirmModal/VestingBalanceWithdraw.jsx
+++ src/components/Modal/TransactionConfirmModal/VestingBalanceWithdraw.jsx
@@ -18,7 +18,7 @@ class VestingBalanceWithdraw extends React.Component {
     let amountValue = asset && operation.amount.amount
       ? operation.amount.amount / Math.pow(10, asset.precision)
       : 0;
-    console.log(amountValue);
+
     let amountFeeValue = asset && operation.fee.amount
       ? operation.fee.amount / Math.pow(10, asset.precision)
       : 0;
