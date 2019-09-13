a4db64f891138e4158f00b8c1ad0a7d15275c30f
mseaward <m.seaward@pbsa.info>
2019-09-13 12:48:29 -0300

    feat: multistep gpos wizard prep

--- src/components/Modal/GPOSWizard/GPOSWizardWrapper.jsx
+++ src/components/Modal/GPOSWizard/GPOSWizardWrapper.jsx
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {GPOSActions} from '../../../actions';
-import Translate from 'react-translate-component';
+// import Translate from 'react-translate-component';
 import Modal from 'react-modal';
 import {getTotalGposBalance} from '../../../selectors/GPOSSelector';
+import Start from './Start';
 
 Modal.setAppElement('#content');
 
@@ -19,10 +20,10 @@ class GPOSWizardWrapper extends Component {
 
   render() {
     let {totalGpos} = this.props;
-    let disablePowerDown = totalGpos && totalGpos > 0 ? false : true;
+    // let disablePowerDown = totalGpos && totalGpos > 0 ? false : true;
 
-    // Override as we do not have gpos withdrawal functionality
-    disablePowerDown = true;
+    // // Override as we do not have gpos withdrawal functionality
+    // disablePowerDown = true;
 
     return (
       <div className='gposModal'>
@@ -33,89 +34,9 @@ class GPOSWizardWrapper extends Component {
           overlayClassName='gpos-modal__bg'
           className='gpos-modal'
         >
-          <div className='gpos-modal__content'>
-            <div className='gpos-modal__content-left'>
-              <div className='gpos-modal__wizard-desc'>
-                <Translate
-                  component='div'
-                  className='title'
-                  content='gpos.wizard.start.desc.title'
-                />
-                <Translate
-                  component='p'
-                  className='txt--bold'
-                  content='gpos.wizard.start.desc.txt-1'
-                />
-                <Translate
-                  component='p'
-                  className='txt--bold'
-                  content='gpos.wizard.start.desc.txt-2'
-                />
-                <Translate
-                  component='p'
-                  className='txt'
-                  content='gpos.wizard.start.desc.txt-3'
-                />
-                <Translate
-                  component='p'
-                  className='txt'
-                  content='gpos.wizard.start.desc.txt-4'
-                />
-                <Translate
-                  component='p'
-                  className='txt--indent txt--bold'
-                  content='gpos.wizard.start.desc.txt-5'
-                />
-                <Translate
-                  component='p'
-                  className='txt--indent txt--bold'
-                  content='gpos.wizard.start.desc.txt-6'
-                />
-                <Translate
-                  component='p'
-                  className='txt--margin-top line'
-                  content='gpos.wizard.start.desc.txt-7'
-                />
-              </div>
-            </div>
-            <div className='gpos-modal__content-right'>
-              <div className='gpos-modal__step'>
-                <img className='gpos-modal__step-1' src='images/gpos/step1.png' alt='step1'/>
-                <Translate
-                  component='p'
-                  className='gpos-modal__step-txt'
-                  content='gpos.wizard.start.right.1'
-                />
-              </div>
-              <div className='gpos-modal__step'>
-                <img className='gpos-modal__step-2' src='images/gpos/step2.png' alt='step2'/>
-                <Translate
-                  component='p'
-                  className='gpos-modal__step-txt'
-                  content='gpos.wizard.start.right.2'
-                />
-              </div>
-              <div className='gpos-modal__step'>
-                <img className='gpos-modal__step-3' src='images/gpos/step3.png' alt='step3'/>
-                <Translate
-                  component='p'
-                  className='gpos-modal__step-txt'
-                  content='gpos.wizard.start.right.3'
-                />
-              </div>
-              <div className='gpos-modal__btns'>
-                <button className='gpos-modal__btn-cancel' onClick={ this.closeModal }>
-                  <Translate className='gpos-modal__btn-txt' content='gpos.wizard.cancel'/>
-                </button>
-                <button className='gpos-modal__btn-up'>
-                  <Translate className='gpos-modal__btn-txt' content='gpos.wizard.up'/>
-                </button>
-                <button className='gpos-modal__btn-down' disabled={ disablePowerDown }>
-                  <Translate className='gpos-modal__btn-txt' content='gpos.wizard.down'/>
-                </button>
-              </div>
-            </div>
-          </div>
+          {
+            <Start totalGpos={ totalGpos } closeModal={ this.closeModal }/>
+          }
         </Modal>
       </div>
     );
---
+++ src/components/Modal/GPOSWizard/Start.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import Translate from 'react-translate-component';
+
+class GposWizardStart extends React.Component {
+  render() {
+    let {totalGpos, closeModal} = this.props;
+    let disablePowerDown = totalGpos && totalGpos > 0 ? false : true;
+
+    // Override as we do not have gpos withdrawal functionality
+    disablePowerDown = true;
+
+    return (
+      <div className='gpos-modal__content'>
+        <div className='gpos-modal__content-left'>
+          <div className='gpos-modal__wizard-desc'>
+            <Translate
+              component='div'
+              className='title'
+              content='gpos.wizard.start.desc.title'
+            />
+            <Translate
+              component='p'
+              className='txt--bold'
+              content='gpos.wizard.start.desc.txt-1'
+            />
+            <Translate
+              component='p'
+              className='txt--bold'
+              content='gpos.wizard.start.desc.txt-2'
+            />
+            <Translate
+              component='p'
+              className='txt'
+              content='gpos.wizard.start.desc.txt-3'
+            />
+            <Translate
+              component='p'
+              className='txt'
+              content='gpos.wizard.start.desc.txt-4'
+            />
+            <Translate
+              component='p'
+              className='txt--indent txt--bold'
+              content='gpos.wizard.start.desc.txt-5'
+            />
+            <Translate
+              component='p'
+              className='txt--indent txt--bold'
+              content='gpos.wizard.start.desc.txt-6'
+            />
+            <Translate
+              component='p'
+              className='txt--margin-top line'
+              content='gpos.wizard.start.desc.txt-7'
+            />
+          </div>
+        </div>
+        <div className='gpos-modal__content-right'>
+          <div className='gpos-modal__step'>
+            <img className='gpos-modal__step-1' src='images/gpos/step1.png' alt='step1'/>
+            <Translate
+              component='p'
+              className='gpos-modal__step-txt'
+              content='gpos.wizard.start.right.1'
+            />
+          </div>
+          <div className='gpos-modal__step'>
+            <img className='gpos-modal__step-2' src='images/gpos/step2.png' alt='step2'/>
+            <Translate
+              component='p'
+              className='gpos-modal__step-txt'
+              content='gpos.wizard.start.right.2'
+            />
+          </div>
+          <div className='gpos-modal__step'>
+            <img className='gpos-modal__step-3' src='images/gpos/step3.png' alt='step3'/>
+            <Translate
+              component='p'
+              className='gpos-modal__step-txt'
+              content='gpos.wizard.start.right.3'
+            />
+          </div>
+          <div className='gpos-modal__btns'>
+            <button className='gpos-modal__btn-cancel' onClick={ closeModal }>
+              <Translate className='gpos-modal__btn-txt' content='gpos.wizard.cancel'/>
+            </button>
+            <button className='gpos-modal__btn-up'>
+              <Translate className='gpos-modal__btn-txt' content='gpos.wizard.up'/>
+            </button>
+            <button className='gpos-modal__btn-down' disabled={ disablePowerDown }>
+              <Translate className='gpos-modal__btn-txt' content='gpos.wizard.down'/>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GposWizardStart;
