10b90093b73571d4df48c4f0333f4b2c8bf252a2
mseaward <m.seaward@pbsa.info>
2020-03-12 13:06:13 -0300

    refactor(witnesslist.jsx): refactor to resolve sonarcloud code smells

--- src/components/Voting/WitnessList.jsx
+++ src/components/Voting/WitnessList.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Translate from 'react-translate-component';
 import {connect} from 'react-redux';
 import WitnessRow from './WitnessRow';
+import Utils from '../../common/utils';
 
 class WitnessListNew extends React.Component {
   state = {
     initialRowData: [],
     rowData: [],
     ranks: [],
-    sortDirections: { // true = ascend, false = descend
+    sortDirections: {
+      // true = ascend, false = descend
       rank: 'default',
       name: 'default',
       last_block: 'default',
@@ -16,66 +18,65 @@ class WitnessListNew extends React.Component {
       total_missed: 'default',
       votes: 'default'
     }
-  }
+  };
 
   componentDidMount() {
     // Get initial data and set to state
-    let {activeWitnesseAccounts, activeWitnesseObjects} = this.props;
+    const {activeWitnesseAccounts, activeWitnesseObjects} = this.props;
     let most_recent_aslot = 0;
     let ranks = {};
     let witnesses = activeWitnesseObjects;
 
-    witnesses.sort((a, b) => {
-      if (a && b) {
-        return parseInt(b.total_votes, 10) - parseInt(a.total_votes, 10);
-      }
-
-      return null;
-    }).forEach( (w, index) => {
-      if (w) {
-        let s = w.last_aslot;
-
-        if (most_recent_aslot < s) {
-          most_recent_aslot = s;
+    witnesses
+      .sort((a, b) => {
+        if (a && b) {
+          return parseInt(b.total_votes, 10) - parseInt(a.total_votes, 10);
         }
 
-        ranks[w.id] = index + 1;
-      }
-    });
+        return null;
+      })
+      .forEach((w, index) => {
+        if (w) {
+          let s = w.last_aslot;
+
+          if (most_recent_aslot < s) {
+            most_recent_aslot = s;
+          }
+
+          ranks[w.id] = index + 1;
+        }
+      });
 
     let itemRows = null;
 
     if (witnesses.size > 0) {
-      itemRows = witnesses.filter((a) => {
-        if (!a) {
-          return false;
-        }
+      itemRows = witnesses
+        .filter((a) => {
+          if (!a) {
+            return false;
+          }
 
-        let account = activeWitnesseAccounts.get(a.witness_account);
+          let account = activeWitnesseAccounts.get(a.witness_account);
 
-        if (!account) {
-          return false;
-        }
+          if (!account) {
+            return false;
+          }
 
-        let name = account.name;
+          let name = account.name;
 
-        if (!name) {
-          return false;
-        }
+          if (!name) {
+            return false;
+          }
 
-        return true;
-      }).map((a) => {
-        let a_account = activeWitnesseAccounts.get(a.witness_account);
+          return true;
+        })
+        .map((a) => {
+          let a_account = activeWitnesseAccounts.get(a.witness_account);
 
-        return (
-          <WitnessRow
-            key={ a.id }
-            rank={ ranks[a.id] }
-            witnessAccount={ a_account }
-            witness={ a }
-          />
-        );
-      });
+          return (
+            <WitnessRow key={ a.id } rank={ ranks[a.id] } witnessAccount={ a_account } witness={ a } />
+          );
+        });
 
       this.setState({
         initialRowData: itemRows,
@@ -90,7 +91,7 @@ class WitnessListNew extends React.Component {
     // Determine column accessor
     let accessor;
 
-    switch(col) {
+    switch (col) {
       case 'rank':
         accessor = 'props.rank';
         break;
@@ -129,14 +130,16 @@ class WitnessListNew extends React.Component {
     }
 
     // Get current sort direction for clicked column header and invert (setState is async so no guarantee it has updated)
-    let sortedItemRows = this.state.rowData.sort((a, b) => {
-      let aValue = this.getAccessor(a, accessor);
-      let bValue = this.getAccessor(b, accessor);
+    const sortedItemRows = this.state.rowData.sort((a, b) => {
+      const aValue = this.getAccessor(a, accessor);
+      const bValue = this.getAccessor(b, accessor);
 
-      if (newSortDir[col]) {
-        return aValue > bValue ? 1 : -1;
-      } else if (!newSortDir[col]) {
-        return aValue < bValue ? 1 : -1;
+      if (Utils.isEmptyObject(aValue) && Utils.isEmptyObject(bValue)) {
+        if (newSortDir[col]) {
+          return aValue > bValue ? 1 : -1;
+        } else if (!newSortDir[col]) {
+          return aValue < bValue ? 1 : -1;
+        }
       }
 
       return this.state.initialRowData; // return initial data
@@ -146,11 +149,11 @@ class WitnessListNew extends React.Component {
       sortDirections: newSortDir, // Invert sort direction
       rowData: sortedItemRows
     });
-  }
+  };
 
   getAccessor = (obj, str) => {
     str = str.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-    str = str.replace(/^\./, '');           // strip a leading dot
+    str = str.replace(/^\./, ''); // strip a leading dot
     var a = str.split('.');
 
     for (var i = 0, n = a.length; i < n; ++i) {
@@ -159,12 +162,12 @@ class WitnessListNew extends React.Component {
       if (k in obj) {
         obj = obj[k];
       } else {
-        return;
+        return {};
       }
     }
 
     return obj;
-  }
+  };
 
   render() {
     const {rowData, sortDirections} = this.state;
@@ -173,16 +176,44 @@ class WitnessListNew extends React.Component {
       <div>
         <div className='table table2 table-voting-witnesses2'>
           <div className='table__head tableRow'>
-            <div className={ `tableCell sort--${sortDirections.rank}` } onClick={ () => this.sortBy('rank') }><Translate content='witnesses.rank'/></div>
-            <div className={ `tableCell sort--${sortDirections.name}` } onClick={ () => this.sortBy('name') }><Translate content='votes.name'/></div>
-            <div className={ `tableCell text_r sort--${sortDirections.last_block}` } onClick={ () => this.sortBy('last_block') }><Translate content='witnesses.last_block'/></div>
-            <div className={ `tableCell text_r sort--${sortDirections.last_confirmed}` } onClick={ () => this.sortBy('last_confirmed') }><Translate content='witnesses.last_confirmed'/></div>
-            <div className={ `tableCell text_r sort--${sortDirections.total_missed}` } onClick={ () => this.sortBy('total_missed') }><Translate content='witnesses.missed'/></div>
-            <div className={ `tableCell text_r sort--${sortDirections.votes}` } onClick={ () => this.sortBy('votes') }><Translate content='votes.votes'/></div>
+            <div
+              className={ `tableCell sort--${sortDirections.rank}` }
+              onClick={ () => this.sortBy('rank') }
+            >
+              <Translate content='witnesses.rank' />
+            </div>
+            <div
+              className={ `tableCell sort--${sortDirections.name}` }
+              onClick={ () => this.sortBy('name') }
+            >
+              <Translate content='votes.name' />
+            </div>
+            <div
+              className={ `tableCell text_r sort--${sortDirections.last_block}` }
+              onClick={ () => this.sortBy('last_block') }
+            >
+              <Translate content='witnesses.last_block' />
+            </div>
+            <div
+              className={ `tableCell text_r sort--${sortDirections.last_confirmed}` }
+              onClick={ () => this.sortBy('last_confirmed') }
+            >
+              <Translate content='witnesses.last_confirmed' />
+            </div>
+            <div
+              className={ `tableCell text_r sort--${sortDirections.total_missed}` }
+              onClick={ () => this.sortBy('total_missed') }
+            >
+              <Translate content='witnesses.missed' />
+            </div>
+            <div
+              className={ `tableCell text_r sort--${sortDirections.votes}` }
+              onClick={ () => this.sortBy('votes') }
+            >
+              <Translate content='votes.votes' />
+            </div>
           </div>
-          <div className='table__body'>
-            {rowData}
-          </div>
+          <div className='table__body'>{rowData}</div>
         </div>
       </div>
     );
@@ -191,9 +222,9 @@ class WitnessListNew extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    current : state.voting.witnesses.currentWitnessId,
-    activeWitnesseObjects : state.voting.witnesses.activeWitnesseObjects,
-    activeWitnesseAccounts : state.voting.witnesses.activeWitnesseAccounts
+    current: state.voting.witnesses.currentWitnessId,
+    activeWitnesseObjects: state.voting.witnesses.activeWitnesseObjects,
+    activeWitnesseAccounts: state.voting.witnesses.activeWitnesseAccounts
   };
 };
 
